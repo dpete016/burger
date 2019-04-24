1dc1c33d4fdc9fc25c8ffe1f5034fda9b911b1d9
Dan <dpete016@gmail.com>
2019-04-24 19:52:20 -0400

    jaws added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "band3D016#",
-    database: "burgers_db",
-});
-
+if (process.env.JAWSB_URL) {
+ connection = mysql.createConnection(process.env.JAWSB_URL);
+} else {
+  var connection = mysql.createConnection({
+     host: "localhost",
+     port: 3306,
+     user: "root",
+     password: "band3D016#",
+     database: "burgers_db",
+    });
+};
 connection.connect(function(err) {
     if(err) {
         console.error("error connecting: " + err.stack);
